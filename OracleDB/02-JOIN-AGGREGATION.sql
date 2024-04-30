928606c9cd4a2cfa3d896735723d04a8f0d18fd7
Sangbeom Shin <aopaoi0987@gmail.com>
2024-04-30 15:13:34 +0900

    study sql (correlated query, subquery, etc.)

--- OracleDB/02-JOIN-AGGREGATION.sql
+++ OracleDB/02-JOIN-AGGREGATION.sql
@@ -311,3 +311,232 @@ WHERE
         FROM
             employees
     );
+
+---------------------------------------------------------------------------------
+
+SELECT
+    department_id,
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    department_id = 110;
+
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    salary IN (12008, 8300);
+
+-- multi-row subquery
+-- 전체 직원 중 110번 부서가 받는 봉급이랑 같은 봉급을 받는 직원?
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    salary IN(
+        SELECT
+            salary
+        FROM
+            employees
+        WHERE
+            department_id = 110
+    );
+
+SELECT
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    salary > ALL(
+        SELECT
+            salary
+        FROM
+            employees
+        WHERE
+            department_id = 110
+    );
+
+---------------------------------------------------------------------------------
+
+-- correlated query
+
+-- 자신이 속한 부서의 평균 급여보다 많이 받는 직원 목록?
+SELECT
+    first_name,
+    salary,
+    department_id
+FROM
+    employees outer
+WHERE
+    salary > (
+        SELECT
+            AVG(salary)
+        FROM
+            employees
+        WHERE
+            department_id = outer.department_id
+    );
+
+-- 각 부서별 최고급여 받는 사원?
+-- 1. Correlated query
+SELECT
+    department_id,
+    first_name,
+    salary
+FROM
+    employees outer
+WHERE
+    salary = (
+        SELECT
+            MAX(salary)
+        FROM
+            employees
+        WHERE
+            department_id = outer.department_id
+    )
+ORDER BY
+    1;
+
+-- 2. GROUP BY
+SELECT
+    department_id,
+    first_name,
+    salary
+FROM
+    employees
+WHERE
+    (department_id, salary) IN (
+        SELECT
+            department_id,
+            MAX(salary)
+        FROM
+            employees
+        GROUP BY
+            department_id
+    )
+ORDER BY
+    1;
+
+-- 3. JOIN
+SELECT
+    outer.department_id,
+    outer.first_name,
+    outer.salary
+FROM
+    employees outer
+    JOIN (
+        SELECT
+            department_id,
+            MAX(salary)   msal
+        FROM
+            employees
+        GROUP BY
+            department_id
+    ) inner
+    ON outer.department_id = inner.department_id
+    AND outer.salary = inner.msal
+ORDER BY
+    1;
+
+---------------------------------------------------------------------------------
+
+
+-- TOP-K Query (using rownum)
+SELECT
+    rownum,
+    first_name,
+    salary,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date >= TO_DATE('170101', 'yymmdd')
+    AND hire_date < TO_DATE('180101', 'yymmdd')
+    AND ROWNUM < 6
+ORDER BY
+    salary DESC;
+
+---------------------------------------------------------------------------------
+
+
+-- 다음과 같이 쓰지않도록 주의
+SELECT
+    first_name
+FROM
+    employees
+UNION
+SELECT
+    department_name
+FROM
+    departments;
+
+-- 입사일조건 & 봉급조건 직원?
+SELECT
+    first_name,
+    salary,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date < TO_DATE('150101', 'yymmdd') INTERSECT
+    SELECT
+        first_name,
+        salary,
+        hire_date
+    FROM
+        employees
+    WHERE
+        salary > 12000;
+
+-- 위 내용은 아래와 같다.
+SELECT
+    first_name,
+    salary,
+    hire_date
+FROM
+    employees
+WHERE
+    hire_date < TO_DATE('150101', 'yymmdd') MINUS
+    SELECT
+        first_name,
+        salary,
+        hire_date
+    FROM
+        employees
+    WHERE
+        salary <= 12000;
+
+---------------------------------------------------------------------------------
+
+-- RANK
+
+SELECT
+    first_name,
+    salary,
+    rank() OVER (ORDER BY salary DESC)       "랭크",
+    dense_rank() OVER (ORDER BY salary DESC) "덴스랭크",
+    row_number() OVER (ORDER BY salary DESC) "행번호",
+    rownum
+FROM
+    employees;
+
+
+-- SELECT distinct department_id, sum(salary) over (order by department_id) from employees;
+
+---------------------------------------------------------------------------------
+
+-- Hierarchical Query
+
+SELECT
+    employee_id, manager_id, first_name, job_id, level
+FROM employees
+START WITH manager_id IS NULL
+CONNECT BY PRIOR employee_id = manager_id
+ORDER BY level;
